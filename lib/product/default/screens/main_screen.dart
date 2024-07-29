f347a4bc816c466ad793e80322b8726ce3e129d6
tradgineer <wnsdudrkwh@gmail.com>
2024-07-29 17:03:22 +0900

    <feat> product default : app_router & main_screen 추가

---
+++ lib/product/default/screens/main_screen.dart
@@ -0,0 +1,75 @@
+// main_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:tickerwatch/product/home/my_home_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedTabIndex = 0;
+
+  final List<Widget> _tabScreens = const [
+    MyHomePage(
+      title: 'my home page',
+    ),
+  ];
+
+// 앱 종료 직전 확인 팝업 노출
+  Future<bool> _showExitPopup(BuildContext context) async {
+    return await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('앱을 종료하시겠습니까?'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('아니요'),
+                onPressed: () => Navigator.of(context).pop(false),
+              ),
+              ElevatedButton(
+                child: const Text('예'),
+                onPressed: () => Navigator.of(context).pop(true),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme currentTheme = Theme.of(context).colorScheme;
+
+    return WillPopScope(
+      onWillPop: () async {
+        return _showExitPopup(context);
+      },
+      child: Scaffold(
+        body: IndexedStack(
+          index: _selectedTabIndex,
+          children: _tabScreens,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTabIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedTabIndex = index;
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+          ],
+          selectedItemColor: currentTheme.primary,
+          unselectedItemColor: currentTheme.secondary,
+        ),
+      ),
+    );
+  }
+}
