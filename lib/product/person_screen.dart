ca3457602e36ceeec7a6009609c63be98e274b84
tradgineer <wnsdudrkwh@gmail.com>
2024-08-02 08:24:09 +0900

    <fix> product person : _selectedGenderController로 gender값 적용해 등록된 person 데이터 edit 화면에서 Gender값과 무관하게 Gender라는 텍스트만 노출되는 문제 해결

--- lib/product/person_screen.dart
+++ lib/product/person_screen.dart
@@ -79,7 +79,7 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
   final _nameController = TextEditingController();
   final _ageController = TextEditingController();
   final _birthDateController = TextEditingController();
-  String? _selectedGender;
+  final _selectedGenderController = TextEditingController();
   String? _nameErrorText;
   String? _ageErrorText;
   String? _birthDateErrorText;
@@ -92,12 +92,13 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
       _nameController.text = widget.person!.name;
       _ageController.text = widget.person!.age.toString();
       _birthDateController.text = widget.person!.birthDate;
-      _selectedGender = widget.person!.gender;
+      _selectedGenderController.text = widget.person!.gender;
     }
 
     _nameController.addListener(_validateInputs);
     _ageController.addListener(_validateInputs);
     _birthDateController.addListener(_validateInputs);
+    _selectedGenderController.addListener(_validateInputs);
 
     // 초기 유효성 검사 호출
     _validateInputs();
@@ -108,9 +109,11 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
     _nameController.removeListener(_validateInputs);
     _ageController.removeListener(_validateInputs);
     _birthDateController.removeListener(_validateInputs);
+    _selectedGenderController.removeListener(_validateInputs);
     _nameController.dispose();
     _ageController.dispose();
     _birthDateController.dispose();
+    _selectedGenderController.dispose();
     super.dispose();
   }
 
@@ -124,8 +127,9 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
       _birthDateErrorText = _birthDateController.text.isEmpty
           ? 'Birth Date cannot be empty'
           : null;
-      _genderErrorText =
-          _selectedGender == null ? 'Gender cannot be empty' : null;
+      _genderErrorText = _selectedGenderController.text.isEmpty
+          ? 'Gender cannot be empty'
+          : null;
     });
   }
 
@@ -146,7 +150,7 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
               title: Text(value),
               onTap: () {
                 setState(() {
-                  _selectedGender = value;
+                  _selectedGenderController.text = value;
                   _validateInputs();
                 });
                 Navigator.pop(context);
@@ -193,10 +197,10 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
             onTap: () => _showGenderBottomSheet(context),
             child: AbsorbPointer(
               child: TextField(
+                controller: _selectedGenderController,
                 decoration: InputDecoration(
                   labelText: 'Gender',
                   errorText: _genderErrorText,
-                  hintText: _selectedGender ?? 'Select Gender',
                 ),
               ),
             ),
@@ -214,11 +218,11 @@ class __PersonDialogState extends ConsumerState<_PersonDialog> {
           onPressed: _isFormValid
               ? () {
                   if (_nameErrorText == null && _ageErrorText == null) {
-                    final name = _nameController.text;
-                    final age = int.tryParse(_ageController.text) ?? 0;
-                    final birthDate = _birthDateController.text;
-                    final gender = _selectedGender!;
-                    final person = Person(
+                    final String name = _nameController.text;
+                    final int age = int.tryParse(_ageController.text) ?? 0;
+                    final String birthDate = _birthDateController.text;
+                    final String gender = _selectedGenderController.text;
+                    final Person person = Person(
                         name: name,
                         age: age,
                         birthDate: birthDate,
