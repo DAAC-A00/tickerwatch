9455d35f873581b12f5905d204e4cae4c72675b0
tradgineer <wnsdudrkwh@gmail.com>
2024-08-07 14:27:45 +0900

    <feat> product default : AOS에서 뒤로가기 버튼으로 앱 종료 시도시, 정말 종료할지 확인하는 Dialog 노출 기능 추가

--- lib/product/default/screens/default_screen.dart
+++ lib/product/default/screens/default_screen.dart
@@ -1,6 +1,8 @@
 // default_screen.dart
 
+import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tickerwatch/product/home/home_main_screen.dart';
 import 'package:tickerwatch/product/setting/screens/setting_main_screen.dart';
 
@@ -22,64 +24,89 @@ class _DefaultScreenState extends State<DefaultScreen> {
     SettingMainScreen(),
   ];
 
-  // 앱 종료 직전 확인 팝업 노출
-  Future<bool> _showExitPopup(BuildContext context) async {
-    return await showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('앱을 종료하시겠습니까?'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('아니요'),
-                onPressed: () => Navigator.of(context).pop(false),
-              ),
-              ElevatedButton(
-                child: const Text('예'),
-                onPressed: () => Navigator.of(context).pop(true),
-              ),
-            ],
+  @override
+  void initState() {
+    super.initState();
+    BackButtonInterceptor.add(myInterceptor);
+  }
+
+  @override
+  void dispose() {
+    BackButtonInterceptor.remove(myInterceptor);
+    super.dispose();
+  }
+
+  bool myInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
+    _showExitDialog();
+    return true; // 뒤로가기 버튼의 기본 동작을 막습니다.
+  }
+
+  Future<void> _showExitDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // 다이얼로그 밖을 터치해도 닫히지 않도록 설정
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('앱 종료'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('앱을 종료하시겠습니까?'),
+              ],
+            ),
           ),
-        ) ??
-        false;
+          actions: <Widget>[
+            TextButton(
+              child: const Text('아니오'),
+              onPressed: () {
+                Navigator.of(context).pop(); // 다이얼로그 닫기
+              },
+            ),
+            TextButton(
+              child: const Text('예'),
+              onPressed: () {
+                Navigator.of(context).pop(); // 다이얼로그 닫기
+                SystemNavigator.pop(); // 앱 종료
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final ColorScheme currentTheme = Theme.of(context).colorScheme;
 
-    return WillPopScope(
-      onWillPop: () async {
-        return _showExitPopup(context);
-      },
-      child: Scaffold(
-        body: IndexedStack(
-          index: _selectedTabIndex,
-          children: _tabScreens,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedTabIndex,
-          onTap: (index) {
-            setState(() {
-              _selectedTabIndex = index;
-            });
-          },
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Person',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Setting',
-            ),
-          ],
-          selectedItemColor: currentTheme.primary,
-          unselectedItemColor: currentTheme.secondary,
-        ),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedTabIndex,
+        children: _tabScreens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTabIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedTabIndex = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Person',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+        selectedItemColor: currentTheme.primary,
+        unselectedItemColor: currentTheme.secondary,
       ),
     );
   }
