6e756a9116b9ccf862bb52d87251fc54d058992c
cjy <junyoung.choi.a@miraeasset.com>
2024-08-19 19:06:51 +0900

    <feat> product watch : 나가기 아이콘 변경, digitalClockColor hintColor로 변경 - hintColor != Grey 확인

--- lib/product/watch/screens/watch_main_screen.dart
+++ lib/product/watch/screens/watch_main_screen.dart
@@ -11,32 +11,37 @@ class WatchMainScreen extends StatelessWidget {
     final ColorScheme currentTheme = Theme.of(context).colorScheme;
 
     return Scaffold(
+      // 내용
       body: Center(
         child: AnalogClock(
           decoration: const BoxDecoration(
             color: Colors.transparent,
             shape: BoxShape.circle,
           ),
+          // 시계 틀
+          tickColor: currentTheme.primary, // 눈금 색상
+          numberColor: currentTheme.primary, // 숫자 색상
+          // 침
           showSecondHand: true, // 초침 표시 여부
           hourHandColor: currentTheme.primary, // 시침 색상
           minuteHandColor: currentTheme.primary, // 분침 색상
           secondHandColor: currentTheme.secondary, // 초침 색상
-          tickColor: currentTheme.primary, // 눈금 색상
-          numberColor: currentTheme.primary, // 숫자 색상
-          showDigitalClock: true, // 디지털 시계 활성화 여부
-          digitalClockColor: currentTheme.secondary,
+          // 디지털 시계
+          showDigitalClock: true, // 활성화 여부
+          digitalClockColor: Theme.of(context).hintColor, // 색
         ),
       ),
+      // 나가기 버튼
       floatingActionButton: RawMaterialButton(
         onPressed: () {
-          Navigator.of(context).pop(); // 뒤로가기
+          Navigator.of(context).pop();
         },
-        shape: const CircleBorder(), // 원형으로 설정
-        fillColor: Colors.transparent, // 배경색 투명으로 설정
+        shape: const CircleBorder(),
+        fillColor: Colors.transparent,
         elevation: 0, // 그림자 제거
         child: Icon(
-          Icons.arrow_back,
-          color: currentTheme.secondary, // 아이콘 색상을 텍스트 색상과 동일하게 설정
+          Icons.output,
+          color: currentTheme.secondary,
         ),
       ),
       floatingActionButtonLocation:
