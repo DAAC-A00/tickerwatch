e85a72c729f443f6b00f9005f3b185533f797265
DAAC-A00 <wnsdudrkwh@gmail.com>
2024-08-26 17:16:59 +0900

    <refact> product tickers : TickerInfoEntity -> TickerInfoModel 변경 및 TickerInfoService 삭제해 코드 통합

--- lib/product/tickers/entities/ticker_info_model.dart
+++ lib/product/tickers/entities/ticker_info_model.dart
@@ -1,4 +1,4 @@
-// ticker_info_entity.dart
+// ticker_info_model.dart
 
 import 'package:hive/hive.dart';
 import 'package:tickerwatch/external/default/exchange_raw_category_enum.dart';
@@ -6,7 +6,7 @@ import 'package:tickerwatch/product/tickers/enums/option_type_enum.dart';
 
 import '../enums/category_exchange_enum.dart';
 
-class TickerInfoEntity {
+class TickerInfoModel {
   // tickerId
   //    무기한 상품 고유값 ex1 : BTC_0/USDT_0
   //    무기한 상품 고유값 ex2 : BTC_1/KRW_0
@@ -56,7 +56,7 @@ class TickerInfoEntity {
   final String remark;
   final String searchKeywords;
 
-  TickerInfoEntity({
+  TickerInfoModel({
     required this.tickerId,
     // raw
     required this.rawSymbol,
@@ -95,16 +95,74 @@ class TickerInfoEntity {
     required this.remark,
     required this.searchKeywords,
   });
+
+  // rawSymbol 예시
+  //    bybit spot : BTCBRL, PEPEUSDC, DOGEEUR, 1SOLUSDT, 1INCHUSDT
+  //    bybit linear : 10000000AIDOGEUSDT, 1000000PEIPEIUSDT, 10000COQUSDT, SHIB1000PERP, BTCPERP, BTC-06SEP24, BTC-25OCT24, BTC-27DEC24, BTC-27JUN25, BTC-27SEP24, BTC-28MAR25, BTC-30AUG24
+  //    bybit inverse : BTCUSDZ24, BTCUSDU24, DOTUSD
+  //    bitget umcbl : BTCUSDT_UMCBL
+  //    bitget dmcbl : BTCUSD_DMCBL, ETHUSD_DMCBL_240927
+  //    bitget cmcbl : ETHPERP_CMCBL
+  //    okx SPOT : BTC-USDT
+  //    okx SWAP : BTC-USD-SWAP, ETH-USDC-SWAP
+  //    okx FUTURES : XRP-USDT-241227
+  //    okx OPTION : BTC-USD-240906-62000-P, BTC-USD-241108-50000-C
+
+  TickerInfoModel? rawToTickerInfo(
+      ExchangeRawCategoryEnum exchangeRawCategoryEnum, String rawSymbol,
+      {String? subData, bool isPreferToFiat = false}) {
+    late List<String> splitSymbol;
+    late String tmpSymbol;
+
+    // 불필요한 rawSymbol의 데이터 삭제해서 tmpSymbol 만들기
+    // bitget의 _$rawCategory 문구 제거
+    switch (exchangeRawCategoryEnum) {
+      case ExchangeRawCategoryEnum.bitgetUmcbl:
+        tmpSymbol = rawSymbol.replaceFirst('_UMCBL', '');
+        break;
+      case ExchangeRawCategoryEnum.bitgetDmcbl:
+        tmpSymbol = rawSymbol.replaceFirst('_DMCBL', '');
+        break;
+      case ExchangeRawCategoryEnum.bitgetCmcbl:
+        tmpSymbol = rawSymbol.replaceFirst('_CMCBL', '');
+        break;
+      case ExchangeRawCategoryEnum.okxSwap:
+        tmpSymbol = rawSymbol.replaceFirst('_SWAP', '');
+        break;
+      default:
+        tmpSymbol = rawSymbol; // 나머지는 rawSymbol 그대로 tmpSymbol로 가져오기
+    }
+
+    // tmpSymbol 예시
+    //    bybit spot : BTCBRL, PEPEUSDC, DOGEEUR, 1SOLUSDT, 1INCHUSDT
+    //    bybit linear : 10000000AIDOGEUSDT, 1000000PEIPEIUSDT, 10000COQUSDT, SHIB1000PERP, BTCPERP, BTC-06SEP24, BTC-25OCT24, BTC-27DEC24, BTC-27JUN25, BTC-27SEP24, BTC-28MAR25, BTC-30AUG24
+    //    bybit inverse : BTCUSDZ24, BTCUSDU24, DOTUSD
+    //    bitget umcbl : BTCUSDT
+    //    bitget dmcbl : BTCUSD, ETHUSD_240927
+    //    bitget cmcbl : ETHPERP
+    //    okx SPOT : BTC-USDT
+    //    okx SWAP : BTC-USD, ETH-USDC
+    //    okx FUTURES : XRP-USDT-241227
+    //    okx OPTION : BTC-USD-240906-62000-P, BTC-USD-241108-50000-C
+
+    splitSymbol =
+        tmpSymbol.contains('_') ? tmpSymbol.split('_') : tmpSymbol.split('-');
+    return null;
+
+    // TODO 이어서 데이터 가공 로직 구현 필요
+
+    // tickerid 예시 : // BTC_0/USDT_0 OR BTC_0/USDT_0-2024y12m20d13h30m
+  }
 }
 
-class TickerInfoEntityAdapter extends TypeAdapter<TickerInfoEntity> {
+class TickerInfoModelAdapter extends TypeAdapter<TickerInfoModel> {
   @override
   final int typeId = 2; // 타입 식별자입니다.
 
   @override
-  TickerInfoEntity read(BinaryReader reader) {
-    // 바이너리 데이터를 읽어 TickerInfoEntity 객체를 생성합니다.
-    return TickerInfoEntity(
+  TickerInfoModel read(BinaryReader reader) {
+    // 바이너리 데이터를 읽어 TickerInfoModel 객체를 생성합니다.
+    return TickerInfoModel(
       tickerId: reader.readString(),
       rawSymbol: reader.readString(),
       symbolSub: reader.readString(),
@@ -140,8 +198,8 @@ class TickerInfoEntityAdapter extends TypeAdapter<TickerInfoEntity> {
   }
 
   @override
-  void write(BinaryWriter writer, TickerInfoEntity obj) {
-    // TickerInfoEntity 객체를 바이너리 데이터로 씁니다.
+  void write(BinaryWriter writer, TickerInfoModel obj) {
+    // TickerInfoModel 객체를 바이너리 데이터로 씁니다.
     writer.writeString(obj.tickerId);
     writer.writeString(obj.rawSymbol);
     writer.writeString(obj.symbolSub);
