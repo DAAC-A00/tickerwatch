f72c18060feda2ffbf3ba9fab4489e3f854aeca6
DAAC-A00 <wnsdudrkwh@gmail.com>
2024-08-29 14:03:22 +0900

    <feat> external bybit : BybitApiManager & BybitAllSpotApiService 초안 작성

--- lib/external/bybit/services/bybit_all_spot_api_service.dart
+++ lib/external/bybit/services/bybit_all_spot_api_service.dart
@@ -1,1 +1,9 @@
+// bybit_all_spot_api_service.dart
 
+import '../../default/exchange_raw_category_enum.dart';
+
+class BybitAllSpotApiService {
+  static ExchangeRawCategoryEnum exchangeRawCategoryEnum =
+      ExchangeRawCategoryEnum.bybitSpot;
+  static String endPoint = exchangeRawCategoryEnum.allTickerListApiEndPoint;
+}
---
+++ lib/external/bybit/services/bybit_api_manager.dart
@@ -0,0 +1,5 @@
+// bybit_api_manager.dart
+
+class BybitApiManager {
+  // TODO api call이 단기간 내에 많이 있으면 문제가 되므로, 각 exchange별로 api call 관리
+}
