3f6ddbb7234872ec83863ed5bba27e63e237af40
DAAC-A00 <wnsdudrkwh@gmail.com>
2024-10-09 19:51:54 +0900

    <fix> external naver : NaverMarketIndexService.getDataList에 try catch 추가하여 인터넷 연결 실패시에도 정상처리되도록 변경

--- lib/external/naver/services/naver_market_index_service.dart
+++ lib/external/naver/services/naver_market_index_service.dart
@@ -1,5 +1,7 @@
 // naver_market_index_service.dart
 
+import 'dart:developer';
+import 'dart:io'; // 추가: 네트워크 관련 예외 처리
 import 'package:html/dom.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
@@ -15,59 +17,72 @@ class NaverMarketIndexService {
   /// 웹 페이지에서 환율 정보를 가져오는 함수
   static Future<List<TickerEntity>?> getDataList() async {
     final url = Uri.parse(urlString);
-    final response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      var document = parser.parse(response.body);
+    try {
+      final response = await http.get(url);
 
-      // <ul> 태그에서 환율 정보 찾기
-      final List<Element> dataList = document.querySelectorAll('.data_lst li');
-      List<TickerEntity> tickerList = [];
+      if (response.statusCode == 200) {
+        var document = parser.parse(response.body);
 
-      for (var data in dataList) {
-        String currencyName = data.querySelector('h3')?.text.trim() ?? '';
-        String value = data.querySelector('.value')?.text.trim() ?? '';
-        String change = data.querySelector('.change')?.text.trim() ?? '';
-        String trend = data.querySelector('.blind')?.text.trim() ?? '';
-        String source = data.querySelector('.source')?.text.trim() ?? '';
-        String time = data.querySelector('.time')?.text.trim() ?? '';
-        String count = data.querySelector('.count .num')?.text.trim() ?? '';
+        // <ul> 태그에서 환율 정보 찾기
+        final List<Element> dataList =
+            document.querySelectorAll('.data_lst li');
+        List<TickerEntity> tickerList = [];
 
-        double? price = double.tryParse(value);
-        double? changePrice = double.tryParse(change);
-        double? changePercentUtc9 = price != null && changePrice != null
-            ? changePrice / (price - changePrice) * 100
-            : null;
-        PriceStatusEnum priceStatusEnum = trend == '상승'
-            ? PriceStatusEnum.up
-            : trend == '하락'
-                ? PriceStatusEnum.down
-                : PriceStatusEnum.stay;
+        for (var data in dataList) {
+          String currencyName = data.querySelector('h3')?.text.trim() ?? '';
+          String value = data.querySelector('.value')?.text.trim() ?? '';
+          String change = data.querySelector('.change')?.text.trim() ?? '';
+          String trend = data.querySelector('.blind')?.text.trim() ?? '';
+          String source = data.querySelector('.source')?.text.trim() ?? '';
+          String time = data.querySelector('.time')?.text.trim() ?? '';
+          String count = data.querySelector('.count .num')?.text.trim() ?? '';
 
-        TickerInfoModel tickerInfo = TickerInfoModel(
-          rawSymbol: currencyName,
-          source: source,
-          remark: count,
-          searchKeywords: 'naverfiatrates$currencyName$source$count',
-        );
-        TickerModel tickerModel = TickerModel(
-          price: value,
-          changePercentUtc9: changePercentUtc9?.toStringAsFixed(2) ?? '',
-          priceStatusEnum: priceStatusEnum,
-          dataAt: time.length > 16
-              ? time.replaceAll('.', '-')
-              : '${time.replaceAll('.', '-')}:00',
-        );
+          double? price = double.tryParse(value.replaceAll(',', ''));
+          double? changePrice = double.tryParse(change.replaceAll(',', ''));
+          double? changePercentUtc9 = price != null && changePrice != null
+              ? changePrice / (price - changePrice) * 100
+              : null;
+          PriceStatusEnum priceStatusEnum = trend == '상승'
+              ? PriceStatusEnum.up
+              : trend == '하락'
+                  ? PriceStatusEnum.down
+                  : PriceStatusEnum.stay;
 
-        // FiatRatesModel 객체 추가 (실제 구현에 맞게 수정 필요)
-        tickerList.add(TickerEntity(
+          TickerInfoModel tickerInfo = TickerInfoModel(
+            rawSymbol: currencyName,
+            source: source,
+            remark: count,
+            searchKeywords: 'naverfiatrates$currencyName$source$count',
+          );
+          TickerModel tickerModel = TickerModel(
+            price: value,
+            changePercentUtc9: changePercentUtc9?.toStringAsFixed(2) ?? '',
+            priceStatusEnum: priceStatusEnum,
+            dataAt: time.length > 16
+                ? time.replaceAll('.', '-')
+                : '${time.replaceAll('.', '-')}:00',
+          );
+
+          // FiatRatesModel 객체 추가 (실제 구현에 맞게 수정 필요)
+          tickerList.add(TickerEntity(
             info: tickerInfo,
             recentData: tickerModel,
-            beforeData: TickerModel(price: '')));
+            beforeData: TickerModel(price: ''),
+          ));
+        }
+        return tickerList;
+      } else {
+        throw Exception('웹 페이지를 불러오지 못했습니다. 상태 코드: ${response.statusCode}');
       }
-      return tickerList;
-    } else {
-      throw Exception('웹 페이지를 불러오지 못했습니다. 상태 코드: ${response.statusCode}');
+    } on SocketException catch (e) {
+      // 소켓 예외 처리
+      log('[ NaverMarketIndexService.getDataList ] SocketException: $e');
+      return null; // 또는 빈 리스트를 반환
+    } catch (e) {
+      // 다른 예외 처리
+      log('[ NaverMarketIndexService.getDataList ] Error: $e');
+      return null;
     }
   }
 }
