4f9cbbcfc652e90556473317a50c6ca6246f2f98
DAAC-A00 <wnsdudrkwh@gmail.com>
2024-10-17 11:10:09 +0900

    <feat> product allticker : AllTickerMainScreen isBorderEnabled 설정값 적용

--- lib/product/allticker/screens/all_ticker_main_screen.dart
+++ lib/product/allticker/screens/all_ticker_main_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tickerwatch/product/allticker/screens/all_ticker_detail_screen.dart';
 import 'package:tickerwatch/product/default/widgets/custom_modal_bottom_sheet.dart';
 import 'package:tickerwatch/product/setting/states/ticker_setting_provider.dart';
+import 'package:tickerwatch/product/tickers/entities/ticker_entity.dart';
 import 'package:tickerwatch/product/tickers/enums/price_status_enum.dart';
 
 import '../../tickers/states/ticker_provider.dart';
@@ -38,12 +39,18 @@ class _AllTickerMainScreenState extends ConsumerState<AllTickerMainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // theme 가져오기
+    final currentTextTheme = Theme.of(context).textTheme;
+    final Color transparentColor = Colors.transparent;
+
+    // ticker 설정 정보 가져오기
     final tickerSetting = ref.watch(tickerSettingProvider);
+    final bool? isBorderEnabled = tickerSetting.isBorderEnabled;
     final Color? upColor = tickerSetting.upColor;
     final Color? downColor = tickerSetting.downColor;
-    final Color transparentColor = Colors.transparent;
+
+    // ticker 데이터 가져오기
     final tickers = ref.watch(tickerProvider);
-    final currentTextTheme = Theme.of(context).textTheme;
 
 // 검색된 ticker 리스트
     final filteredTickers = tickers.where((ticker) {
@@ -88,22 +95,34 @@ class _AllTickerMainScreenState extends ConsumerState<AllTickerMainScreen> {
           : ListView.builder(
               itemCount: filteredTickers.length,
               itemBuilder: (context, index) {
-                final ticker = filteredTickers[index];
+                final TickerEntity ticker = filteredTickers[index];
                 double currentPrice =
                     double.tryParse(ticker.recentData.price) ?? 0.0;
                 double previousPrice =
                     double.tryParse(ticker.beforeData.price) ?? 0.0;
 
+                bool isUpdatedRecently = ticker.recentData.isUpdatedRecently;
 // 테두리 색상 결정
-                Color borderColor = transparentColor;
-                if (ticker.recentData.isUpdatedRecently &&
-                    currentPrice > previousPrice) {
-                  borderColor = upColor ?? transparentColor; // 가격 상승
-                } else if (ticker.recentData.isUpdatedRecently &&
-                    currentPrice < previousPrice) {
-                  borderColor = downColor ?? transparentColor; // 가격 하락
+                late Color borderColor;
+                if (isBorderEnabled != null) {
+                  // isBorderEnabled 설정값 가져온 경우
+                  if (isBorderEnabled) {
+                    // 설정에서 테두리 깜빡임 활성화된 경우
+                    if (isUpdatedRecently && currentPrice > previousPrice) {
+                      borderColor = upColor ?? transparentColor; // 가격 상승
+                    } else if (isUpdatedRecently &&
+                        currentPrice < previousPrice) {
+                      borderColor = downColor ?? transparentColor; // 가격 하락
+                    } else {
+                      borderColor = transparentColor; // 가격 동일하거나 깜빡임 종료
+                    }
+                  } else {
+                    // 설정에서 테두리 깜빡임 비활성화된 경우
+                    borderColor = transparentColor;
+                  }
                 } else {
-                  borderColor = transparentColor; // 가격 동일
+                  // isBorderEnabled 설정값 가져오지 않은 경우
+                  borderColor = transparentColor;
                 }
 
 // changePercent 결정 및 글자 색상 설정
@@ -161,18 +180,25 @@ class _AllTickerMainScreenState extends ConsumerState<AllTickerMainScreen> {
                       ),
                       Expanded(
                         flex: 1,
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal:
-                                (currentTextTheme.bodyMedium?.fontSize ?? 18) /
-                                    3,
-                          ),
-                          child: FittedBox(
-                            fit: BoxFit.scaleDown, // 텍스트가 길어질 경우 크기 조정
-                            alignment: Alignment.centerRight, // 오른쪽 정렬
-                            child: Text(
-                              changePercent,
-                              style: TextStyle(color: textColor),
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown, // 텍스트가 길어질 경우 크기 조정
+                          alignment: Alignment.centerRight, // 오른쪽 정렬
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: transparentColor, width: 1),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal:
+                                    (currentTextTheme.bodyMedium?.fontSize ??
+                                            18) /
+                                        3,
+                              ),
+                              child: Text(
+                                changePercent,
+                                style: TextStyle(color: textColor),
+                              ),
                             ),
                           ),
                         ),
